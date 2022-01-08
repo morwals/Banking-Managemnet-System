41f6f11a4e980c770ca894a9f2f50087d509dc15
morwals <sumit.123gm.com@gmail.com>
2022-01-08 22:15:55 +0530

    final

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,8 @@
 const mongoose=require("mongoose");
 
-module.exports = mongoose.connect(process.env.MONGO_URI).then(()=>{
+// process.env.MONGO_URI
+
+module.exports = mongoose.connect("mongodb+srv://sumit:sumit123@cluster0.hgwri.mongodb.net/bankDB2?retryWrites=true&w=majority").then(()=>{
     console.log("db connected");
 }).catch(err=>{
     console.log("error in db");
